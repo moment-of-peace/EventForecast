fb5ec53d0d83433b14669cc8de508fa4bc0cbbd3
Charles Lee <charlee940321@gmail.com>
2017-09-10 09:52:51 +1000

    fix some bugs

--- datacleaning.py
+++ datacleaning.py
@@ -1,58 +1,58 @@
 import logger
 '''
-    This file contains a set of functions that is helpful for cleaning data and copy designated data
-    into the new files
+    Thisfilecontainsasetoffunctionsthatishelpfulforcleaningdataandcopydesignateddata
+    intothenewfiles
 '''
-logpath = 'cleaning.log'
+logpath='cleaning.log'
 
 '''
-    this function is used for extracting the events that are satisfied with the set value of specified column
+    thisfunctionisusedforextractingtheeventsthataresatisfiedwiththesetvalueofspecifiedcolumn
     parameters:
-        fname: the name of readed file
-        oname: the name of writed file
-        col: the index of the column
-        value: the value of colnum
-        writerpath: the path of the directory of cleaned data
-        @return line_count: the count of copied lines
+        fname:thenameofreadedfile
+        oname:thenameofwritedfile
+        col:theindexofthecolumn
+        value:thevalueofcolnum
+        writerpath:thepathofthedirectoryofcleaneddata
+        @returnline_count:thecountofcopiedlines
 '''
 
 def get_specified_data(fname,oname,col,value,count):
-    line_count = 0
-    reader = open(fname, 'r', encoding='utf-8')
-    writer = open(oname, 'a', encoding='utf-8')
+    line_count=0
+    reader=open(fname,'r',encoding='utf-8')
+    writer=open(oname,'a',encoding='utf-8')
     for line in reader:
-        columns = line.split('\t')
+        columns=line.split('\t')
         for i in col:
-            if columns[i] == value:
+            if columns[i]==value:
                 writer.write(line)
-                line_count += 1
+        line_count+=1
     reader.close()
     writer.close()
-    logger.log(str(fname + '  '+'current count is:' + str(count)),logpath)
+    logger.log(str(fname+''+'currentcount is:'+str(count)),logpath)
     return line_count
 
 '''
-    function for extracting the events happened in the wanted countries
+    functionforextractingtheeventshappenedinthewantedcountries
     parameter:
-        fname: the name of the file that if to be read
-        newfname: the name of output file
-        idxs: the columns that should be excluded in the output files
-        @return line_count
+        fname:thenameofthefilethatiftoberead
+        newfname:thenameofoutputfile
+        idxs:thecolumnsthatshouldbeexcludedintheoutputfiles
+        @returnline_count
 '''
 
 def del_cvs_col(fname,newfname,idxs,count):
-    line_count = 0
-    reader = open(fname, 'r', encoding='utf-8')
-    writer = open(newfname, 'a', encoding='utf-8')
+    line_count=0
+    reader=open(fname,'r',encoding='utf-8')
+    writer=open(newfname,'a',encoding='utf-8')
     for line in reader:
-        items = line.split('\t')
-        string = ''
-        print(len(items))
+        items=line.split('\t')
+        string=''
         for i in range(0,len(items)):
             if i not in idxs:
-                string += items[i]+'\t'
+                string = string + items[i]+'\t'
         writer.write(string+'\n')
-        line_count += 1
+        line_count+=1
     writer.close()
     reader.close()
+    logger.log(str(fname+''+'currentcount is:'+str(count)),logpath)
     return line_count
--- event_main.py
+++ event_main.py
@@ -2,13 +2,17 @@ import datacleaning
 import os
 import logger
 data_cleaning_logpath = 'cleaning.log'
-flist = os.listdir('datafiles/')
+flist = os.listdir('datafiles/201304now/')
 count = 0
 count1 = 0
 for f in flist:
-    tmp_count = datacleaning.get_specified_data('datafiles/'+f,'filtered/ausdata.csv',[36],'Australia',count)
-    count = tmp_count
-    tmp_count = datacleaning.del_cvs_col('datafiles/'+f,'cleaned/'+f,[57],count1)
+    tmp_count = datacleaning.del_cvs_col('datafiles/201304now/'+f,'cleaned/2013/'+f,[57],count1)
     count1 = tmp_count
+
+flist1 = os.listdir('cleaned/2013/')
+for f1 in flist1:
+	tmp_count = datacleaning.get_specified_data('datafiles/2013/'+f1,'filtered/2013/ausdata.csv',[36],'Australia',count)
+	count = tmp_count
+	
 logger.log('final count is:' + str(count),data_cleaning_logpath)
 logger.log('final count is:' + str(count1),data_cleaning_logpath)
