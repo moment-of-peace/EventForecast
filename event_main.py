f6fe6430899d79678ca607bcfbcb4b037c4d774a
moment-of-peace <yi.liu1183@gmail.com>
2017-09-10 14:07:31 +1000

    integrate preprocessing functions

--- event_main.py
+++ event_main.py
@@ -1,18 +1,10 @@
-import datacleaning
-import os
-import logger
-data_cleaning_logpath = 'cleaning.log'
-flist = os.listdir('datafiles/201304now/')
-count = 0
-count1 = 0
-for f in flist:
-    tmp_count = datacleaning.del_cvs_col('datafiles/201304now/'+f,'cleaned/2013/'+f,[57],count1)
-    count1 = tmp_count
+import preprocessing as pr
 
-flist1 = os.listdir('cleaned/2013/')
-for f1 in flist1:
-	tmp_count = datacleaning.get_specified_data('datafiles/2013/'+f1,'filtered/2013/ausdata.csv',[36],'Australia',count)
-	count = tmp_count
-	
-logger.log('final count is:' + str(count),data_cleaning_logpath)
-logger.log('final count is:' + str(count1),data_cleaning_logpath)
+columnToDel = [0,2,3,4,53,54,56,57]
+
+clean_file_path = pr.del_columns('data-2013/', columnToDel)
+print('cleaned file path: ' + clean_file_path)
+
+# notice: after delete, column index of a attr will change !
+attr_path = pr.get_specified_data(clean_file_path,[32],'Australia',22)
+print('attr file path' + attr_path)
--- logger.py
+++ logger.py
@@ -2,4 +2,4 @@ import datetime
 def log(s, logpath):
 	with open(logpath, 'a', encoding='utf-8') as logfile:
 		logfile.write(s+' '+str(datetime.datetime.now())+'\n')
-		logfile.close()
+		# if use with...as, don't need logfile.close()
