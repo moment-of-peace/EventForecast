7a7f6e1a66e404c26665eaa77728bf7a0c9f9cc5
moment-of-peace <yi.liu1183@gmail.com>
2017-10-06 06:33:14 +1000

    modify rnn model

--- rnn_model.py
+++ rnn_model.py
@@ -2,11 +2,13 @@ import numpy as np
 import os
 
 import keras.layers as kl
+import keras.models as km
 
+DIM = 20
 
 # build a vector to represent the event type distibution
 def build_event_vec(fileName):
-    vec = np.zeros(20, dtype=np.int32)
+    vec = np.zeros(DIM, dtype=np.int32)
     with open(fileName, 'r') as f:
         line = f.readline()
         line = f.readline().strip('\n')
@@ -22,7 +24,7 @@ def build_event_vec(fileName):
 # build a raw dataset containing a series of event type distribution
 def build_event_dataset(path, start=0, end=None):
     flist = os.listdir(path)  # all files in the directory
-    dataset = np.zeros((end-start, 20), dtype=np.int32)
+    dataset = np.zeros((end-start, DIM), dtype=np.int32)
     if end == None:
         end = len(flist)
     for i in range(start, end):
@@ -30,29 +32,42 @@ def build_event_dataset(path, start=0, end=None):
     return dataset
 
 # build dataset used for rnn training
-def build_train_set(path, size, step):
-    dim = 20
-    trainSet = np.zeros((size, step, dim), dtype=np.int32)
-    for i in range(size):
-        trainSet[i,:,:] = build_event_dataset(path, i, i+step)
+def build_train_set(path, start, end, step):
+    trainSet = np.zeros((end-start, step, DIM), dtype=np.int32)
+    for i in range(start, end):
+        trainSet[i-start,:,:] = build_event_dataset(path, i, i+step)
     return trainSet
 
 # build dataset used for rnn test
-def build_train_result(path, size, step):
-    dim = 20
-    trainSet = np.zeros((size, 1, dim), dtype=np.int32)
-    for i in range(size):
-        trainSet[i,:,:] = build_event_dataset(path, i+step, i+step+1)
-    return trainSet
+def build_train_result(path, start, end, step):
+    trainResult = np.zeros((end-start, 1, DIM), dtype=np.int32)
+    for i in range(start, end):
+        trainResult[i-start,:,:] = build_event_dataset(path, i+step, i+step+1)
+
+    trainResult = np.reshape(trainResult, (end-start,DIM))    
+    return trainResult
 
 path = 'attr'
-trainData = build_train_set(path, 1500, 30)
-trainResult = build_train_result('attr', 1500, 30)
-
+trainSize = 1500
+testSize = 60
+step = 30
+# build training and test datasets
+trainData = build_train_set(path, 0, trainSize, step)
+trainResult = build_train_result(path, 0, trainSize, step)
+testData = build_train_set(path, trainSize, trainSize+testSize, step)
+testResult = build_train_result(path, trainSize, trainSize+testSize, step)
+# tain model
 model = km.Sequential()
-model.add(kl.LSTM(20, input_shape=(30,20), activation='sigmoid'))
-model.add(kl.Dense(20))
+model.add(kl.LSTM(DIM, input_shape=(DIM, step), activation='sigmoid'))
+model.add(kl.Dense(DIM))
 model.compile(loss='mean_squared_error', optimizer='adam')
-trainResult = np.reshape(trainResult, (1500,20))
-model.fit(trainData, trainResult, epochs=100, batch_size=10, verbose=2)
+model.fit(trainData, trainResult, epochs=500, batch_size=10, verbose=1)
+# predict and evaluate
+predict = model.predict(testData)
+errorSum = 0
+for i in range(testSize):
+    diff = sum(predict[i,:] - testResult[i,:]) / sum(testResult[i,:])
+    errorSum = errorSum + diff * diff
+errorAve = errorSum / testSize
+print('average error: %f'%(errorAve))
 
